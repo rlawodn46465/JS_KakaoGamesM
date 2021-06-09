85862698d91c38f0bb83559283df1031ffeade69
rlawodn46465 <rlawodn46465@naver.com>
2021-06-09 13:59:11 +0900

    1차 완성

--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@
 var slideIndex = 0;
 var slide = document.getElementById("slide_img");
 
-window.onload = function(){
-var sec = 4000;
+window.onload = function () {
+  var sec = 4000;
 
-setInterval(function(){
-  slideIndex++;
-  showSlides(slideIndex);
-}, sec);
+  setInterval(function () {
+    slideIndex++;
+    showSlides(slideIndex);
+  }, sec);
 }
 
-function moveSlides(n){
+function moveSlides(n) {
   slideIndex = slideIndex + n
   showSlides(slideIndex);
 }
 
-function showSlides(n){
+function showSlides(n) {
   var slides = document.getElementById("slide_img");
   var size = slides.childElementCount;
   var slide_width = slides.firstElementChild.offsetWidth;
 
-  if((n + 1) > size){
+  if ((n + 1) > size) {
     slideIndex = 0;
     n = 0;
-  } else if(n < 0){
+  } else if (n < 0) {
     slideIndex = (size - 1);
     n = (size - 1);
   }
@@ -33,6 +33,85 @@ function showSlides(n){
 }
 
 /* 공지사항 */
+let rollingData = [
+  '<카카오게임즈 개인정보처리방침>이 2018년 05월 24일자로 변경 됩니다.',
+  '대작 모바일 MMORPG \'오딘:발할라 라이징\'캐릭터 생성 서버 2차 증설!',
+  '카카오게임즈, 모바일 RPG\'가디언 테일즈\', 신규 영웅 \'해변의 과학자 소히\' 업데이트!',
+  '카카오게임즈, 코로나19 백신 접종 유급 휴가 시행!',
+  '대작 모바일 MMORPG \'오딘:발할라 라이징\'캐릭터 생성 서버 증설!',
+  '대작 모바일 MMORPG \'오딘:발할라 라이징\'6월 29일 정식 출시!',
+  '카카오게임즈 \'프린세스 커넥트! Re:Dive\', 신규 캐릭터 \'린(레인저\'전격 등장!',
+  '카카오게임즈, \'이터널 리턴\' 30번째 신규 캐릭터 \'일레븐\' 출시!',
+  '카카오게임즈 핵심 계열사, 프렌즈게임즈-웨이투빗 합병',
+  '카카오게임즈, 대작 모바일 MMORPG \'오딘: 발할라 라이징\' AR과 XR접목한 업계 최초 \'버츄얼 쇼케이스\' 예고!',
+  '카카오게임즈, 모바일 MMORPG \'달빛조각사\' 5월 25일, 미국 일본 등 글로벌 그랜드 출시!',
+  '카카오게임즈, 모바일 RPG\'가디언테일즈\', \신규 영웅 \'괴도 루시\' 업데이트!'
+] // 공지 리스트
+
+let timer = 2000
+
+let first = document.getElementById('first'),
+  second = document.getElementById('second'),
+  third = document.getElementById('third')
+let move = 2
+let dataCnt = 1
+let listCnt = 1
+
+first.children[0].innerHTML = rollingData[0]
+
+setInterval(() => {
+  if (move == 2) {
+    first.classList.remove('notice_card')
+    first.classList.add('notice_card_after')
+
+    second.classList.remove('notice_card_after')
+    second.classList.add('notice_card')
+
+    third.classList.remove('notice_card_after')
+    third.classList.remove('notice_card')
+
+    move = 0
+  } else if (move == 1) {
+    first.classList.remove('notice_card_after')
+    first.classList.add('notice_card')
+
+    second.classList.remove('notice_card_after')
+    second.classList.remove('notice_card')
+
+    third.classList.remove('notice_card')
+    third.classList.add('notice_card_after')
+
+    move = 2
+  } else if (move == 0) {
+    first.classList.remove('notice_card_after')
+    first.classList.remove('notice_card')
+
+    second.classList.remove('notice_card')
+    second.classList.add('notice_card_after')
+
+    third.classList.remove('notice_card_after')
+    third.classList.add('notice_card')
+
+    move = 1
+  }
+
+  if (dataCnt < (rollingData.length - 1)) {
+    document.getElementById('notice_list').children[listCnt].children[0].innerHTML = rollingData[dataCnt]
+    dataCnt++
+  } else if (dataCnt == rollingData.length - 1) {
+    document.getElementById('notice_list').children[listCnt].children[0].innerHTML = rollingData[dataCnt]
+    dataCnt = 0
+  }
+
+  if (listCnt < 2) {
+    listCnt++
+  } else if (listCnt == 2) {
+    listCnt = 0
+  }
+
+}, timer);
+
+/*게임 리스트 */
 
 
 
@@ -171,7 +250,6 @@ function selectAll(selectAll) { //전체 클릭시
   })
 
   if (selectAll.checked == true) {
-    console.log("1번")
     checkboxs.forEach((check) => {
       check.parentNode.classList.add('backcolor');
     });
@@ -179,7 +257,6 @@ function selectAll(selectAll) { //전체 클릭시
       on.style.display = "block"
     });
   } else {
-    console.log("2번")
     checkboxs.forEach((check) => {
       check.parentNode.classList.remove('backcolor');
     });
@@ -194,7 +271,6 @@ function moreOpen() {
   const gameBox = document.querySelector("#game_box");
   gameBox.style.height = "auto";
   moreBox.style.display = "none";
-  console.log("클릭!");
 }
 
 /* tab box */
@@ -206,7 +282,7 @@ const tab3 = document.getElementById("tab3");
 
 
 
-tab[0].addEventListener('click', function(){
+tab[0].addEventListener('click', function () {
   tab1.style.display = "block";
   tab2.style.display = "none";
   tab3.style.display = "none";
@@ -218,7 +294,7 @@ tab[0].addEventListener('click', function(){
   tabLine[2].style.left = "50%"
 });
 
-tab[1].addEventListener('click', function(){
+tab[1].addEventListener('click', function () {
   tab2.style.display = "block"
   tab1.style.display = "none";
   tab3.style.display = "none";
@@ -230,7 +306,7 @@ tab[1].addEventListener('click', function(){
   tabLine[2].style.left = "50%"
 });
 
-tab[2].addEventListener('click', function(){
+tab[2].addEventListener('click', function () {
   tab3.style.display = "block"
   tab1.style.display = "none";
   tab2.style.display = "none";
